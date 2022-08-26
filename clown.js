a688fff1005154758f8ebff567d3d9011e785436
Valter Sinik <valter.sinik@learnet.se>
2022-08-26 15:37:10 +0200

    javascript

---
+++ clown.js
@@ -0,0 +1,16 @@
+console.log("Hej") // sträng
+console.log(2) // nummer
+console.log(true) // boolean
+console.log(false) // boolean
+
+console.log(1+2); // mathematico
+console.log(1*2); // mathematico
+console.log(1/3); // mathematico
+
+var username = "Peetah"
+console.log(username)
+username = "Louise"
+console.log(username)
+console.log("")
+var robot = "Hello world"
+console.log("Hello world \n I was falling in love \n Now I'm only falling apart")
