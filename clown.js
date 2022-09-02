5295e04b82e98badbc0f0871bda49460eb087a4b
Valter Sinik <valter.sinik@learnet.se>
2022-09-02 15:42:43 +0200

    javascript pt2

--- clown.js
+++ clown.js
@@ -1,16 +1,30 @@
-console.log("Hej") // sträng
-console.log(2) // nummer
-console.log(true) // boolean
-console.log(false) // boolean
-
-console.log(1+2); // mathematico
-console.log(1*2); // mathematico
-console.log(1/3); // mathematico
-
 var username = "Peetah"
 console.log(username)
 username = "Louise"
 console.log(username)
 console.log("")
+
+// 1 - 3
 var robot = "Hello world"
 console.log("Hello world \n I was falling in love \n Now I'm only falling apart")
+
+// 4
+var message = "Live and sleep";
+console.log(message);
+
+// 5
+var name = "Ada Lovelace!"
+console.log("Hej "  + name)
+
+const PromptSync = require("prompt-sync")
+
+// 6
+var ps = require("prompt-sync");
+var prompt = ps()
+
+let names = prompt("Enter your name: ");
+console.log("your name is: " + names)
+
+// 7
+var hello = prompt("Give input! ")
+console.log(hello.repeat(3 \ ))
